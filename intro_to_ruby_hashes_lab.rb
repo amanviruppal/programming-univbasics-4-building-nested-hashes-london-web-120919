54fa5f1a8766f7dc689e4d1bcc8f4603a71e7242
amanvir uppal <amanvir.uppal@icloud.com>
2019-10-30 15:42:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ end
 
 def monopoly_with_fourth_tier
   hash = {:railroads => 
-  {:pieces => 4, :names => {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline_railroad => {"mortgage_value" => "800"}},:rent_in_dollars => 
+  {:pieces => 4, :names => {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline_railroad => {"mortgage_value" => "$800"}},:rent_in_dollars => 
   {:four_pieces_owned => 200, :three_pieces_owned => 100, :two_pieces_owned => 50, :one_piece_owned => 25,}}}
     
     
